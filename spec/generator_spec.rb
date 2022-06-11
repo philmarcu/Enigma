d8dbaa0e66badea79a69a4f608f15eede13b5e1f
philmarcu <philmark18@gmail.com>
2022-06-11 15:43:22 -0600

    Feat: create add_keys and shift_hash methods/tests for generator class

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,22 +1,31 @@
 require './lib/generator.rb'
 
 RSpec.describe Generator do
-    let(:gen) {Generator.new}
+	let(:gen) {Generator.new}
 
-    it 'exists' do
-        expect(gen).to be_a(Generator) 
-        expect(gen.key).to eq(nil)
-        expect(gen.date_key).to eq(nil)
-    end
+	it 'exists & has attributes' do
+			expect(gen).to be_a(Generator) 
+			expect(gen.letters).to be_a(Array)
+	end
 
-    it 'can create a new key' do
-        expect(gen.new_key).to be_a(Array)
-        expect(gen.new_key.size).to eq(4)
-        expect(gen.new_key.first.size).to eq(2)
-    end
+	it 'can create a new key' do
+			expect(gen.new_key).to be_a(Array)
+			expect(gen.new_key.size).to eq(4)
+	end
 
-    it 'can create a new date key' do
-        expect(gen.new_date).to be_a(Array)
-        expect(gen.new_date.size).to eq(4)
-    end
+	it 'can create new offsets from the date' do
+			expect(gen.new_date).to be_a(Array)
+			expect(gen.new_date.size).to eq(4)
+	end
+
+	it 'can add new keys and date keys together' do
+			gen.add_keys
+			expect(gen.add_keys.size).to eq(4)
+	end
+
+	it 'has a shift hash of final offsets' do
+			expect(gen.shift_hash).to be_a(Hash)
+			expect(gen.shift_hash.size).to eq(4)
+			expect(gen.shift_hash.values.first).to be_a(Integer)
+	end
 end
