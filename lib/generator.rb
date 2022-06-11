97a06492cea7e28c3d28dfac0515db6cdd6ebebd
philmarcu <philmark18@gmail.com>
2022-06-11 10:53:27 -0600

    Feat: create generator class & test with new key and date methods

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,6 +1,24 @@
+require 'Date'
+
 class Generator
+
+    attr_reader :key, :date_key
+    def initialize
+        @key = nil
+        @date_key = nil
+    end
+
     def new_key
-        5.times.map { rand(0..9) }.join
+        random = Array.new(5) { rand(0..9) }.join
+        numbers = (random.split("")).map {|num| num.to_i}
+        numbers.each_cons(2).to_a
+    end
+
+    def new_date
+        date = Date.today
+        str = (date.strftime("%d%m%Y").to_i ** 2).to_s
+        digits = str[-4..-1]
+        digits.split("").map {|num| num.to_i}
     end
 end
 
