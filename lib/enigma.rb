35756d8d505abe656e013589f07a0a9f0e0f0438
philmarcu <philmark18@gmail.com>
2022-06-12 15:43:52 -0600

    Feat: create decrypt method & 1st decrypt test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,8 @@
 require 'Date'
-require './lib/generator.rb'
 require './lib/offset.rb'
 require './lib/shift.rb'
 
 class Enigma
-  include Generator
 
   attr_reader :key, :date
   def initialize
@@ -12,14 +10,29 @@ class Enigma
     @date = nil
   end
 
-  def encrypt(message, key = new_key, date = Date.today )
+  def new_key
+    Array.new(5) { rand(0..9) }.join
+  end
+
+  def encrypt(message, key = new_key, date = Date.today)
     hash = {}
     shifts = Offset.new(key, date).shift_format
     date = (Offset.new(key, date).date_check).to_s
 
-    hash[:message] = Shift.new.forward(message, shifts)
+    hash[:encryption] = Shift.new.forward(message, shifts)
     hash[:key] = key
     hash[:date] = date
     hash
   end
+
+  def decrypt(ciphertext, key, date = Date.today)
+    hash = {}
+    shifts = Offset.new(key, date).shift_format
+    date = (Offset.new(key, date).date_check).to_s
+
+    hash[:decryption] = Shift.new.backward(ciphertext, shifts)
+    hash[:key] = key
+    hash[:date] = date
+    hash
+  end  
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,4 +1,3 @@
-require './lib/generator.rb'
 require './lib/shift.rb'
 require './lib/offset.rb'
 require './lib/enigma.rb'
@@ -10,13 +9,18 @@ RSpec.describe Enigma do
     expect(enigma).to be_a(Enigma)
   end
 
-  it 'can encrypt a message' do
+  it 'can create a key' do
+    expect(enigma.new_key).to be_a(String)
+    expect(enigma.new_key.size).to eq(5)
+  end
+
+  it 'can encrypt a encryption' do
     message = "Hello World!"
     key = "02715"
     date = "040895"
     
     expected = {
-      message: "keder ohulw!",
+      encryption: "keder ohulw!",
       key: "02715",
       date: "040895"
     }
@@ -29,11 +33,31 @@ RSpec.describe Enigma do
     date = Date.today.strftime("%d%m%Y")
 
     expected = {
-      message: "rijdydugapb!",
+      encryption: "rijdydugapb!",
       key: "02715",
       date: date
     }
 
     expect(enigma.encrypt(message, key)).to eq(expected)
+  end
+
+  it 'can encrypt a message w/o a given key or date' do
+    message = "Hello World!"
+
+    expect(enigma.encrypt(message).size).to eq(3)
+  end
+
+  it 'can decrypt a message' do
+    ciphertext = "keder ohulw!"
+    key = "02715"
+    date = "040895"
+
+    expected = {
+      decryption: "hello world!",
+      key: "02715",
+      date: "040895"
+    }
+
+    expect(enigma.decrypt(ciphertext, key, date)).to eq(expected)
   end
 end
