aea71d3a5b01c6cea722bb0ea948f391a3174461
philmarcu <philmark18@gmail.com>
2022-06-12 09:40:54 -0600

    Refactor: Actually add in generator module

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Offset do
 	let(:set) {Offset.new("02715")}
 
 	it 'exists & has attributes' do
-			expect(set).to be_a(Offset) 
-			expect(set.letters).to be_a(Array)
-			expect(set.key).to eq("02715")
-			expect(set.date).to be_a(Date)
+		expect(set).to be_a(Offset) 
+		expect(set.letters).to be_a(Array)
+		expect(set.key).to eq("02715")
+		expect(set.date).to be_a(Date)
 	end
 
 	it 'can be Datetime object or String' do
@@ -18,13 +18,13 @@ RSpec.describe Offset do
 	end
 
 	it 'can create a new key' do
-			expect(set.key_format).to be_a(Array)
-			expect(set.key_format.size).to eq(4)
+		expect(set.key_format).to be_a(Array)
+		expect(set.key_format.size).to eq(4)
 	end
 
 	it 'can create new offsets from the date' do
-			expect(set.date_check).to be_a(Integer)
-			expect(set.date_check.size).to eq(8)
+		expect(set.date_check).to be_a(Integer)
+		expect(set.date_check.size).to eq(8)
 	end
 
 	it 'can format the date with a date object' do
@@ -37,8 +37,8 @@ RSpec.describe Offset do
 	end
 
 	it 'can format a string key' do
-			expect(set.shift_format.size).to eq(4)
-			expect(set.shift_format).to eq([10, 31, 79, 19])
+		expect(set.shift_format.size).to eq(4)
+		expect(set.shift_format).to eq([10, 31, 79, 19])
 	end
 
 	it 'has a shift hash of final offsets' do
@@ -47,9 +47,10 @@ RSpec.describe Offset do
 							B: 31,
 							C: 79,
 							D: 19
-		}
-			expect(set.shift_hash).to be_a(Hash)
-			expect(set.shift_hash.size).to eq(4)
-			expect(set.shift_hash).to eq(expected)
+					}
+
+		expect(set.shift_hash).to be_a(Hash)
+		expect(set.shift_hash.size).to eq(4)
+		expect(set.shift_hash).to eq(expected)
 	end
 end
